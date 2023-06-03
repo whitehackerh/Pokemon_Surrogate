479995b4574630d718a7c21a0f271c26bfe03992
whitehackerh <hibiki.takahashi0801@gmail.com>
2023-06-04 00:24:27 +0900

    BaseClass

---
+++ BackEnd/BackEndApi/api/Responders/BaseResponder.py
@@ -0,0 +1,34 @@
+from rest_framework.response import Response
+from datetime import datetime
+
+class BaseResponder:
+    def __init__(self, api_name, e = None):
+        self.responseParameter = {}
+        self.statusCode = 200
+        self.apiName = api_name
+        self.successful = 'success'
+        self.responseCode = '000'
+        self.data = {}
+
+        if e:
+            self.statusCode = 400
+            self.successful = 'failure'
+            self.responseCode = e['code']
+            self.data['errors'] = e['message']
+        
+    def setResponse(self, data):
+        self.responseParameter['responseCode'] = self.responseCode
+        self.responseParameter['time'] = self.getTime()
+        self.responseParameter['apiname'] = self.apiName
+        self.responseParameter['successful'] = self.successful
+        if self.successful == 'success':
+            self.responseParameter['data'] = data
+        else:
+            self.responseParameter['data'] = self.data
+    
+    def getResponse(self):
+        return Response(self.responseParameter)
+
+    def getTime():
+        return datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+
