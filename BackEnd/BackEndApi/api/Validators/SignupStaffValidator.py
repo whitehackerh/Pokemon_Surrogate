6fdfd5b8ef43d9ba9816715d56c30c7cffa81f42
whitehackerh <hibiki.takahashi0801@gmail.com>
2023-06-12 22:33:00 +0900

    #0005 signupStaff

---
+++ BackEnd/BackEndApi/api/Validators/SignupStaffValidator.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from api.models import Users
+from api.Enums.Messages import Messages
+from api.Enums.ResponseCodes import ResponseCodes
+from api.Exceptions.CustomExceptions import CustomExceptions
+
+class SignupStaffValidator(serializers.Serializer):
+    password = serializers.CharField(max_length=128)
+    username = serializers.CharField(max_length=150)
+    first_name = serializers.CharField(max_length=150)
+    last_name = serializers.CharField(max_length=150)
+    email = serializers.EmailField()
+    nickname = serializers.CharField(max_length=255)
+    bank_account = serializers.CharField(max_length=100)
+
+    def customValidation(self, request):
+        self.validateUsername(request.get('username'))
+        self.validateEmail(request.get('email'))
+    
+    def validateUsername(self, value):
+        if Users.objects.filter(username=value).exists():
+            raise CustomExceptions(Messages.USERNAME_ALREADY_USE, ResponseCodes.VALIDATION_ERROR)
+
+    def validateEmail(self, value):
+        if Users.objects.filter(email=value).exists():
+            raise CustomExceptions(Messages.EMAIL_ALREADY_USE, ResponseCodes.VALIDATION_ERROR)
+
--- BackEnd/BackEndApi/api/urls.py
+++ BackEnd/BackEndApi/api/urls.py
@@ -4,11 +4,13 @@ from api.Views.SignupView import SignupView
 from api.Views.LoginView import LoginView
 from api.Views.LogoutView import LogoutView
 from api.Views.SetUserProfileView import SetUserProfileView
+from api.Views.SignupStaffView import SignupStaffView
 
 urlpatterns = [
     path('test', TestAPIView.as_view(), name='test'),
     path('signup', SignupView.as_view(), name='signup'),
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-    path('setUserProfile', SetUserProfileView.as_view(), name='setUserProfile')
+    path('setUserProfile', SetUserProfileView.as_view(), name='setUserProfile'),
+    path('signupStaff', SignupStaffView.as_view(), name='signupStaff')
 ]
